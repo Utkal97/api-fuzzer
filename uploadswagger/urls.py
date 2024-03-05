ab593b96cb4b2f90baac852a7be5b3bba41d8639
lmadipa <lmadipa@clemson.edu>
2024-03-04 19:03:41 -0500

    UI for Restler

--- uploadswagger/urls.py
+++ uploadswagger/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("", views.upload, name="upload"),
+    path("runTest/", views.runTest, name="runTest"),
+    path("runFuzzLean/", views.runFuzzLean, name="runFuzzLean"),
+    path("runFuzz/", views.runFuzz, name="runFuzz"),
     path("results", views.results, name="results"),
 ]
+
--- uploadswagger/views.py
+++ uploadswagger/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import SwaggerFileForm
+import os
+from django.conf import settings
 
 def results(request):
     context = {}
@@ -27,12 +29,38 @@ def results(request):
         }
     return render( request, "uploadswagger/results.html", context)
 
+
+def runTest(request):
+    test_folder_path = "uploadswagger/swagger_files/Test"
+    json_files = []
+
+    for file in os.listdir(test_folder_path):
+        if(file.endsWith('.json')):
+            file_path = os.path(test_folder_path, file)
+            with open(file_path, 'r') as f:
+                json_data = json.load(f)
+            json_files.append(json_data)
+    return render(request, 'upload.html', {'json_data': json_data})
+
 def upload(request):
     if request.method == 'POST':
-        form = SwaggerFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('results')
-    else:
-        form = SwaggerFileForm()
-    return render(request, 'uploadswagger/upload.html', {'form': form})
+        uploaded_swagger_file = request.FILES['swagger_file']
+        download_path = os.path.join('uploadswagger/swagger_files', uploaded_swagger_file.name)
+        with open(download_path, 'wb+') as newfile:
+            for chunk in uploaded_swagger_file.chunks():
+                newfile.write(chunk)
+        print(newfile)
+#         restler_dll_path = "/Users/laxmanmadipadige/Documents/restler-fuzzer/restler_bin/restler/Restler.dll"
+#         command_line_path = f"compile --api_spec {download_path}"
+#         subprocess.run(["dotnet" , restler_dll_path, command_line_path])
+        return render(request, 'uploadswagger/upload.html')
+    return render(request, 'uploadswagger/upload.html')
+
+#     if request.method == 'POST':
+#         form = SwaggerFileForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('results')
+#     else:
+#         form = SwaggerFileForm()
+#     return render(request, 'uploadswagger/upload.html', {'form': form})
