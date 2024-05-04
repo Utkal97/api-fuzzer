8fa0292f8da5c7d904f1fd8610d9ba74436caee7
Utkal97 <utkal.s15@iiits.in>
2024-05-03 23:14:17 -0400

    Added utility functions to parse error text file

---
+++ uploadswagger/utilities.py
@@ -0,0 +1,63 @@
+
+def filterAndGroupLines(lines):
+    groupedLines = []
+
+    currentLine = {}
+
+    for line in lines:
+        if ("->" in line):
+            currentLine["endPoint"] = line
+        elif ("PREVIOUS RESPONSE" in line):
+            currentLine["response"] = line
+        elif ("producer_timing_delay" in line) or ("max_async_wait_time" in line):
+            pass
+        else:
+            if "endPoint" in currentLine and "response" in currentLine:
+                groupedLines.append(currentLine)
+            currentLine = {}
+
+    return groupedLines
+
+
+def parseChecker(filename=""):
+
+    # Remove this filepath and call the function with required filepath
+    filename = "../media/example_files/DataDrivenChecker_20x_1.txt"
+    f = open(filename, "r")
+
+    try:
+        if f.readable()==False:
+            raise Exception("File is not readable")
+        
+        reportLines = f.readlines()
+        
+        reportedError = filterAndGroupLines(reportLines)
+        errors = []
+
+        for group in reportedError:
+            
+            endPointItems = group['endPoint'].split(" ")
+            endPointHttpMethod = endPointItems[1]
+            endPoint = endPointItems[2]
+
+            responseItems = group['response'].split(" ")
+            responseCode = responseItems[3]
+            responseValue = responseItems[5]
+            
+            error = {
+                'httpMethod': endPointHttpMethod,
+                'endPoint': endPoint, 
+                'statusCode': responseCode, 
+                'response': responseValue
+                }
+
+            errors.append(error)
+            
+        return errors
+        
+    except Exception as error:
+        print(error)
+    finally:
+        f.close()
+
+#parseChecker()
